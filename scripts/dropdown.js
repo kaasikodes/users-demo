24d28daedda195bb65b5130a4fb42645dc62b06f
kaasikodes <thisbroisfresh@gmail.com>
2023-08-23 00:16:31 +0100

    fix: dropdown interaction

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -6,10 +6,17 @@ button.addEventListener("click", () => {
   dropdown.classList.toggle("hidden");
 });
 
-button.addEventListener("mouseover", () => {
+button.addEventListener("focus", () => {
   dropdown.classList.remove("hidden");
 });
 
 dropdown.addEventListener("mouseleave", () => {
   dropdown.classList.add("hidden");
+});
+
+// Close the dropdown when clicking outside
+document.addEventListener("click", (e) => {
+  if (!button.contains(e.target) && !dropdown.contains(e.target)) {
+    dropdown.classList.add("hidden");
+  }
 });
